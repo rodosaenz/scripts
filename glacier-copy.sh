5b676ccf57ee976c3f7624dd7c556abb5eda6e95
Rodolfo Sáenz <1944379+rodosaenz@users.noreply.github.com>
2020-04-05 02:27:35 -0500

    Added double quotes at copy statement due spaces

--- glacier-copy.sh
+++ glacier-copy.sh
@@ -7,6 +7,6 @@ INPUT="files.txt"
 while IFS= read -r line
 do
   	FILE=$line
-	RESULT=$(aws s3 cp s3://$BUCKET_SRC/$FILE s3://$BUCKET_DST/$FILE --storage-class STANDARD)
+	RESULT=$(aws s3 cp "s3://$BUCKET_SRC/$FILE" "s3://$BUCKET_DST/$FILE" --storage-class STANDARD)
 	echo "$RESULT $FILE"
 done < "$INPUT"
